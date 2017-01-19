5d69f7b8952bc58784d34137795c9e0d63a845b5
yehsk8rz <fyeh@ucmerced.edu>
2017-01-19 13:30:22 -0800

    Add files via upload
    first draft of script

---
+++ DCVibPrelim/VibrationMotorControl/VibrationMotorControl.ino
@@ -0,0 +1,20 @@
+//Initialize motor pin
+int motorPin = 3;
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
